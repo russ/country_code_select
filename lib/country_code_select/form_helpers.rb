46f887e403d40d1e52b833fb18d947f1fd934627
charly <charlysisto@gmail.com>
2009-04-10 22:37:01 +0800

    fixed : to many arguments in instanceTag.new
    Signed-off-by: Russ Smith <russ@bashme.org>

--- lib/country_code_select/form_helpers.rb
+++ lib/country_code_select/form_helpers.rb
@@ -1,7 +1,7 @@
 module CountryCodeSelect
 	module FormHelpers
 		def country_code_select(object_name, method, priority_countries = nil, options = {})
-			ActionView::Helpers::InstanceTag.new(object_name, method, self, nil, options.delete(:object)).to_country_code_select_tag(priority_countries, options)
+			ActionView::Helpers::InstanceTag.new(object_name, method, self, options.delete(:object)).to_country_code_select_tag(priority_countries, options)
 		end
 	end
 end
